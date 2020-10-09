cd5467ec12ff421facdd0b2613e094af9f1b44d6
rctaylor157 <rctaylor157@outlook.com>
2020-10-09 11:17:49 -0400

    creating class that governs actually playing game

--- Scrabble.py
+++ Scrabble.py
@@ -30,3 +30,31 @@ class ScrabblePlayer():
         player_frame = Tk.Frame(master=player_window)
 
         player_window.mainloop()
+
+class ScrabbleGame():
+    def __init__(self)
+    active_player = str
+    
+    def player_letters_display():
+        letters_display = tk.Tk()
+        letters_display.title("Letters")
+        for i in range(7):
+            letters_display.columnconfigure(i, weight=1)
+            letters_display.rowconfigure(1, weight=1)
+            frame = Frame.Tk(master=letters_display)
+            letter_tile = Label.Tk(master=frame, text=active_player.letter[i])
+        
+        letters_display.mainloop()
+        
+    def score_word(word):
+        word_score = 0
+        word = word.upper()
+        is_valid = False
+        for item in valid_words_list:
+            if word == item:
+                is_valid = True
+                for letter in word:
+                    word_score += letters_to_points.get(letter, 0)
+                return word_score
+        if is_valid == False:
+            return 'Not a valid word!'
